58ce4d01a66e1648919daff4e18624d8b5be2c48
Jcozman <jcozman@gmail.com>
2019-03-10 18:33:09 -0400

    it works

---
+++ GodotTools/Extension/NodeExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Godot;
+
+namespace GodotTools.Extension
+{
+    public static class NodeExtensions
+    {
+        public static Node HAFUCKITWORKS(this Node n, int idx)
+        {
+            return n.GetParent().GetChild(idx);
+        }
+    }
+}
