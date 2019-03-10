f3220bd1822e8e35cbff79e42b9426423988f2a7
Jcozman <jcozman@gmail.com>
2019-03-10 18:46:34 -0400

    add first extension

--- GodotTools/Extension/NodeExtensions.cs
+++ GodotTools/Extension/NodeExtensions.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Reflection;
 using Godot;
 
 namespace GodotTools.Extension
 {
     public static class NodeExtensions
     {
-        public static Node HAFUCKITWORKS(this Node n, int idx)
+        public static void AutoGetNodes(this Node n)
         {
-            return n.GetParent().GetChild(idx);
+            var flags = BindingFlags.DeclaredOnly | BindingFlags.NonPublic | BindingFlags.Instance;
+            var type = n.GetType();
+            var fields = type.GetFields(flags);
+            foreach (var field in fields)
+            {
+                if (field.FieldType == typeof(NodePath))
+                {
+                    var name = field.Name;
+                    var target = name.Substr(0, name.Length - 4);
+                    var node = n.GetNode(field.GetValue(n) as NodePath);
+                    var targetField = type.GetField(target, flags);
+                    if (targetField == null)
+                    {
+                        throw new Exception($"Target field with name \"{target}\" was not found.");
+                    }
+                    targetField.SetValue(n, node);
+                }
+            }
         }
     }
 }
