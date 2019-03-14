7662eed017a550522392b27057b694d13a5bb54f
Jcozman <jcozman@gmail.com>
2019-03-13 21:56:07 -0400

    add disconnect all signals

---
+++ GodotTools/src/Util/NodeUtil.cs
@@ -0,0 +1,26 @@
+﻿using Godot;
+
+namespace GodotTools.Util
+{
+    public static class NodeUtil
+    {
+        public static void DisconnectAllSignals(Node source, Node target)
+        {
+            var signalList = source.GetSignalList();
+            foreach (var signal in signalList)
+            {
+                var dict = (Godot.Collections.Dictionary) signal;
+                var signalName = (string) dict["name"];
+                var connectedSignalList = source.GetSignalConnectionList(signalName);
+                foreach (var connectedSignal in connectedSignalList)
+                {
+                    var connectedDict = (Godot.Collections.Dictionary) connectedSignal;
+                    if (connectedDict["target"] == target)
+                    {
+                        source.Disconnect((string) connectedDict["signal"], target, (string) connectedDict["method"]);
+                    }
+                }
+            }
+        }
+    }
+}
