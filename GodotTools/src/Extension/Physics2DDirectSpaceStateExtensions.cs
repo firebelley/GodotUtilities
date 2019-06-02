2bedccb5782beb6115f39179818155c430a5656c
Jcozman <Jcozman@users.noreply.github.com>
2019-06-02 13:55:47 -0400

    add comment

--- GodotTools/src/Extension/Physics2DDirectSpaceStateExtensions.cs
+++ GodotTools/src/Extension/Physics2DDirectSpaceStateExtensions.cs
@@ -5,6 +5,17 @@ namespace GodotTools.Extension
 {
     public static class Physics2DDirectSpaceStateExtensions
     {
+        /// <summary>
+        /// Returns a RaycastResult if there is a collision, otherwise returns null.
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        /// <param name="exclude"></param>
+        /// <param name="collisionLayer"></param>
+        /// <param name="collideWithBodies"></param>
+        /// <param name="collideWithAreas"></param>
+        /// <returns></returns>
         public static RaycastResult Raycast(this Physics2DDirectSpaceState state, Vector2 from, Vector2 to, Godot.Collections.Array exclude = null, int collisionLayer = int.MaxValue, bool collideWithBodies = true, bool collideWithAreas = false)
         {
             var raycastDict = state.IntersectRay(from, to, exclude, collisionLayer, collideWithBodies, collideWithAreas);
