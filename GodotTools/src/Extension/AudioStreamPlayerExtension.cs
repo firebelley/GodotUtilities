cf692ebbef0411f0d23ae61df4da8ced24708654
Jcozman <Jcozman@users.noreply.github.com>
2019-06-02 10:41:45 -0400

    add resource extension

--- GodotTools/src/Extension/AudioStreamPlayerExtension.cs
+++ GodotTools/src/Extension/AudioStreamPlayerExtension.cs
@@ -4,9 +4,15 @@ namespace GodotTools.Extension
 {
     public static class AudioStreamPlayerExtension
     {
-        public static void Play(this AudioStreamPlayer audioStreamPlayer, float minPitchScale, float maxPitchScale)
+        public static void PlayWithPitchRange(this AudioStreamPlayer audioStreamPlayer, float minPitchScale, float maxPitchScale)
         {
             audioStreamPlayer.PitchScale = (float) GD.RandRange(minPitchScale, maxPitchScale);
+            audioStreamPlayer.Play();
+        }
+
+        public static void PlayWithPitch(this AudioStreamPlayer audioStreamPlayer, float pitchScale)
+        {
+            audioStreamPlayer.PitchScale = pitchScale;
             audioStreamPlayer.Play();
         }
     }
---
+++ GodotTools/src/Extension/ResourcePreloaderExtension.cs
@@ -0,0 +1,33 @@
+using Godot;
+using GodotTools.Util;
+
+namespace GodotTools.Extension
+{
+    public static class ResourcePreloaderExtension
+    {
+        /// <summary>
+        /// Instances a scene with the resource name. Returns null if resource was not found or was not a packed scene.
+        /// </summary>
+        /// <param name="preloader"></param>
+        /// <param name="name"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T InstanceScene<T>(this ResourcePreloader preloader, string name) where T : Node
+        {
+            if (!preloader.HasResource(name))
+            {
+                Logger.Error("Preloader did not have a resource with name " + name);
+                return null;
+            }
+
+            var resource = preloader.GetResource(name) as PackedScene;
+            if (resource == null)
+            {
+                Logger.Error("Resource with name " + name + " was not a " + nameof(PackedScene));
+                return null;
+            }
+
+            return resource.Instance() as T;
+        }
+    }
+}
