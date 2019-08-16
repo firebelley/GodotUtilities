7b3fef86b5c9a18820ed7161637c4047a06e7930
firebelley <firebelley@users.noreply.github.com>
2019-08-15 22:00:39 -0400

    update logger again

--- GodotTools/src/Util/Logger.cs
+++ GodotTools/src/Util/Logger.cs
@@ -6,13 +6,14 @@ namespace GodotTools.Util
     {
         public static void Error(params object[] what)
         {
-            GD.PrintErr("[ERROR]", what);
+
+            GD.PrintErr(what);
             GD.PrintStack();
         }
 
         public static void Info(params object[] what)
         {
-            GD.PrintRaw("[INFO]", what);
+            GD.PrintRaw(what);
         }
     }
 }
