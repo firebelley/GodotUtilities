a742db122b9b880c8dc3a00b9bc360eefba4944b
firebelley <firebelley@users.noreply.github.com>
2020-11-17 20:06:40 -0500

    update extension

--- GodotUtilities/src/Extension/ObjectExtension.cs
+++ GodotUtilities/src/Extension/ObjectExtension.cs
@@ -14,18 +14,23 @@ namespace GodotUtilities
             var signalList = source.GetSignalList();
             foreach (var signal in signalList)
             {
-                var dict = (Godot.Collections.Dictionary) signal;
-                var signalName = (string) dict[DICT_NAME];
+                var dict = (Godot.Collections.Dictionary)signal;
+                var signalName = (string)dict[DICT_NAME];
                 var connectedSignalList = source.GetSignalConnectionList(signalName);
                 foreach (var connectedSignal in connectedSignalList)
                 {
-                    var connectedDict = (Godot.Collections.Dictionary) connectedSignal;
+                    var connectedDict = (Godot.Collections.Dictionary)connectedSignal;
                     if (connectedDict[DICT_TARGET] == target)
                     {
-                        source.Disconnect((string) connectedDict[DICT_SIGNAL], target, (string) connectedDict[DICT_METHOD]);
+                        source.Disconnect((string)connectedDict[DICT_SIGNAL], target, (string)connectedDict[DICT_METHOD]);
                     }
                 }
             }
+        }
+
+        public static T InstanceOrDefault<T>(this Object _, Object obj) where T : class
+        {
+            return Object.IsInstanceValid(obj) ? obj as T : default;
         }
     }
 }
