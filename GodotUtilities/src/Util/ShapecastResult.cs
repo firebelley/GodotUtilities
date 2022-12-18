9400aa43b99d7dbf008f723aa8beab9d6adddbf2
Firebelley <firebelley@users.noreply.github.com>
2022-12-18 13:03:49 -0500

    add shapecast result

---
+++ GodotUtilities/src/Util/ShapecastResult.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+namespace GodotUtilities.Util
+{
+    public class ShapecastResult
+    {
+        public Vector2 Point { get; set; }
+        public Vector2 Normal { get; set; }
+        public Object Collider { get; set; }
+        public int ColliderId { get; set; }
+        public RID RID { get; set; }
+        public int Shape { get; set; }
+        public Vector2 FromPosition { get; set; }
+        public Vector2 ToPosition { get; set; }
+        public Vector2 LinearVelocity { get; set; }
+
+        public ShapecastResult(Vector2 from, Vector2 to, Godot.Collections.Dictionary resultDict)
+        {
+            FromPosition = from;
+            ToPosition = to;
+            Point = (Vector2)resultDict["point"];
+            Normal = (Vector2)resultDict["normal"];
+            Collider = (Object)resultDict["collider"];
+            ColliderId = (int)resultDict["collider_id"];
+            RID = (RID)resultDict["rid"];
+            Shape = (int)resultDict["shape"];
+            LinearVelocity = (Vector2)resultDict["linear_velocity"];
+        }
+    }
+}
