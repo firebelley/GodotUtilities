625d1674cda61d747d3fcd28486622216f783bf9
Firebelley <firebelley@users.noreply.github.com>
2023-02-07 17:33:12 -0500

    update for godot beta 17

--- GodotUtilities/src/Extension/VectorExtension.cs
+++ GodotUtilities/src/Extension/VectorExtension.cs
@@ -10,8 +10,8 @@ namespace GodotUtilities
         public static float ApproximateLength(this Vector2 vec)
         {
             var absVec = vec.Abs();
-            var min = Mathf.Min(absVec.x, absVec.y);
-            var max = Mathf.Max(absVec.x, absVec.y);
+            var min = Mathf.Min(absVec.X, absVec.Y);
+            var max = Mathf.Max(absVec.X, absVec.Y);
             return (ALPHA * max) + (BETA * min);
         }
 
