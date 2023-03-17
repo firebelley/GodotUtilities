b0020074a73b8470e75e1d51daaf98b1ef139ad6
Firebelley <firebelley@users.noreply.github.com>
2023-03-17 19:41:32 -0400

    add tween extension

---
+++ src/Extension/TweenExtension.cs
@@ -0,0 +1,15 @@
+using Godot;
+
+namespace GodotUtilities
+{
+    public static class TweenExtension
+    {
+        public static void KillIfValid(this Tween tween)
+        {
+            if (tween.IsValid())
+            {
+                tween.Kill();
+            }
+        }
+    }
+}
