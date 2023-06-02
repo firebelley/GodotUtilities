aac0b86747bf76d5d2fdeab7d99868b6720edf42
Firebelley <firebelley@users.noreply.github.com>
2023-06-01 21:48:12 -0400

    fix ProjectSettingsExtended.cs

--- GodotUtilities/src/ProjectSettingsExtended.cs
+++ GodotUtilities/src/ProjectSettingsExtended.cs
@@ -7,11 +7,19 @@ namespace GodotUtilities;
 
 public static class ProjectSettingsExtended
 {
-    // TODO: check if this casting of variant works
-    public static T GetSettingOrDefault<T>(string name) => 
-        ProjectSettings.HasSetting(name) ? 
-            (T)(object)ProjectSettings.GetSetting(name) : default;
+    /// <summary>
+    /// Returns the given project setting, or default if the setting doesn't exist.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T GetSettingOrDefault<T>(string name) => ProjectSettings.HasSetting(name) ? ProjectSettings.GetSetting(name).As<T>() : default;
 
-    public static T GetDebugSettingOrDefault<T>(string name) => 
-        OS.IsDebugBuild() ? GetSettingOrDefault<T>(name) : default;
+    /// <summary>
+    /// Returns the given project setting if not a debug build. Returns default value if a debug build or if the setting doesn't exist.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public static T GetDebugSettingOrDefault<T>(string name) => OS.IsDebugBuild() ? GetSettingOrDefault<T>(name) : default;
 }
